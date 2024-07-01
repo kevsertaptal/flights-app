5e9f691bd67ed3347f9b809ec0eb57faddfca6b0
Kevser Taptal <taptaldevelopment@gmail.com>
2024-07-01 18:46:21 +0300

    chore: Add Vuex dependency and update tsconfig to include store file

---
+++ resources/js/store.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import { createStore } from "vuex";
+
+// Create a new store instance.
+const store = createStore({
+    state() {
+        return {
+            count: 0,
+        };
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+    },
+});
+
+// Export the store instance.
+export default store;
